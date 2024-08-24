6645cc7b17282e7d84e926bf67d18d9179ec17fc
Ravi kant <Ravikant02111@gmail.com>
2024-08-24 22:33:14 +0530

    added: OCP assignment solution

--- SOLID principles/Single Responsibility Principle/Invoice.ts
+++ SOLID principles/Single Responsibility Principle/Invoice.ts
@@ -1,17 +1,58 @@
 import { Product } from "./order";
 
-export class Invoice {
-    generateInvoice(products: Product[], amount: number) {
-        console.log(`
-Invoice Date: ${new Date().toDateString()}
--------------------------------
-Product Name\tPrice
-                `);
+interface IInvoiceStrategy {
 
-        products.forEach((product) => {
-            console.log(`${product.name}\t\t${product.price}`);
-        });
-        console.log('-------------------------------');
-        console.log(`Total: ${amount}`);
+    generateInvoice(products: Product[], amount: number): void;
+
+}
+export class Invoice {
+
+    private invoiceStrategy: IInvoiceStrategy;
+
+    constructor(invoiceStrategy: IInvoiceStrategy) {
+
+        this.invoiceStrategy = invoiceStrategy;
+
+    }
+
+    generateInvoice(products: Product[], amount: number) {
+        
+        this.invoiceStrategy.generateInvoice(products, amount);
+
+    }
+}
+
+export class SimpleInvoiceStrategy implements IInvoiceStrategy {
+
+    generateInvoice(products: Product[], amount: number): void {
+        
+        console.log(`
+            Invoice Date: ${new Date().toDateString()}
+            -------------------------------
+            Product Name\tPrice
+                            `);
+            
+                    products.forEach((product) => {
+                        console.log(`${product.name}\t\t${product.price}`);
+                    });
+                    console.log('-------------------------------');
+                    console.log(`Total: ${amount}`);
+        
+    }
+
+}
+
+export class SummaryInvoiceStrategy implements IInvoiceStrategy {
+
+    generateInvoice(products: Product[], amount: number): void {
+
+        console.log(`
+            Invoice Date: ${new Date().toDateString()}
+            -------------------------------
+            Number of Products: ${products.length}
+            -------------------------------
+            Total Amount: ${amount}
+                    `);
+        
     }
 }
--- SOLID principles/Single Responsibility Principle/srp.ts
+++ SOLID principles/Single Responsibility Principle/srp.ts
@@ -1,4 +1,4 @@
-import { Invoice } from './Invoice';
+import { Invoice, SimpleInvoiceStrategy, SummaryInvoiceStrategy } from './Invoice';
 import { Order, Product } from './order';
 import { PaymentProcessor } from './PaymentProcessor';
 import { PricingCalcutor } from './PricingCalculator';
@@ -12,10 +12,15 @@ order.addProduct(product2);
 
 const products = order.getProducts();
 
+// Calculating price
 const pricingCalculator = new PricingCalcutor();
 const total = pricingCalculator.calculatePricing(products);
 
-const invoice = new Invoice();
+// Generating invoice
+const simpleInvoiceStrategy = new SimpleInvoiceStrategy();
+const summaryInvoiceStrategy = new SummaryInvoiceStrategy();
+
+const invoice = new Invoice(summaryInvoiceStrategy);
 invoice.generateInvoice(products, total);
 
 const paymentProcessor = new PaymentProcessor();
